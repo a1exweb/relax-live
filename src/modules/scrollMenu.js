82aefba1acbc582064d5922f911d20294cafc170
Alexander Yaroslavtsev <a1exweb@mail.ru>
2021-03-23 12:42:33 +0300

    add scroll menu

--- src/modules/scrollMenu.js
+++ src/modules/scrollMenu.js
@@ -1,7 +1,6 @@
 const scrollMenu = () => {
     window.addEventListener('scroll', () => {
         const header = document.querySelector('.header');
-        console.log(window);
         if (window.scrollY > 10) {
             header.classList.add('header_active');
         } else {
