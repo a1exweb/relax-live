02be8bd29fc252aacfeebddb0c5cde6008ae0354
Alexander Yaroslavtsev <a1exweb@mail.ru>
2021-03-24 10:08:40 +0300

    change scrollMenu

--- src/modules/scrollMenu.js
+++ src/modules/scrollMenu.js
@@ -1,12 +1,29 @@
 const scrollMenu = () => {
+    const header = document.querySelector('.header');
+    const checkResponseHeighHeader = () => {
+        if (innerWidth > 575) {
+            header.style.height = `120px`;
+        } else {
+            header.style.height = `80px`;
+        }
+    }
+    checkResponseHeighHeader();
+    window.addEventListener('resize', checkResponseHeighHeader);
     window.addEventListener('scroll', () => {
-        const header = document.querySelector('.header');
+        const wrapperMiddle = document.querySelector('.wrapper_middle');
+        let headerHeight = header.style.height;
+        console.log(headerHeight)
+        
         if (window.scrollY > 10) {
             header.classList.add('header_active');
+            wrapperMiddle.style.cssText = `padding-top: ${headerHeight};`;
+            console.log(headerHeight);
         } else {
             header.classList.remove('header_active');
+            wrapperMiddle.style.cssText = `padding-top: unset;`;
         }
     });
+
 };
 
 export default scrollMenu;
