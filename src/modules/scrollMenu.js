6c1807bdc0f2b82343d868c72bbf64f718c0d5cf
Alexander Yaroslavtsev <a1exweb@mail.ru>
2021-03-24 10:33:28 +0300

    build

--- src/modules/scrollMenu.js
+++ src/modules/scrollMenu.js
@@ -12,12 +12,9 @@ const scrollMenu = () => {
     window.addEventListener('scroll', () => {
         const wrapperMiddle = document.querySelector('.wrapper_middle');
         let headerHeight = header.style.height;
-        console.log(headerHeight)
-        
         if (window.scrollY > 10) {
             header.classList.add('header_active');
             wrapperMiddle.style.cssText = `padding-top: ${headerHeight};`;
-            console.log(headerHeight);
         } else {
             header.classList.remove('header_active');
             wrapperMiddle.style.cssText = `padding-top: unset;`;
